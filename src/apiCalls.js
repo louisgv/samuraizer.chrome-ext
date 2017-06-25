4b29945f1aec5fefb80dc325e4810aaa26972c08
Aishwary Pramanik <aishwarypramanik@gmail.com>
2017-06-24 18:32:40 -0700

    Getting keywords done again

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@
  */
 function getKeywords(articleContent)
 {
-    articleContent = '{ "documents": [ {"language": "en", "id": "1", "text": "Love, love, love this sofa!!! Had my eye on it for a long time and finally was able to see and sit on one before purchasing. Article is becoming well known here in the midwest! Customer service sent me leather samples and I put them all through testing: chocolate, ketchup, spilled red wine and the leather cleaned off looking like new. Ordering was a breeze and the delivery company couldn\'t have been more accommodating. I look forward to purchasing again from Article."} ]}'
+    // articleContent = '{ "documents": [ {"language": "en", "id": "1", "text": "Love, love, love this sofa!!! Had my eye on it for a long time and finally was able to see and sit on one before purchasing. Article is becoming well known here in the midwest! Customer service sent me leather samples and I put them all through testing: chocolate, ketchup, spilled red wine and the leather cleaned off looking like new. Ordering was a breeze and the delivery company couldn\'t have been more accommodating. I look forward to purchasing again from Article."} ]}'
     url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases" ;
     fetch(url, { method: 'POST',
         body: articleContent,
